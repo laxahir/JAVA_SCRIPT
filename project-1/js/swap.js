1903544ef59a1f25b5ba76090bb8608a6c0be01e
lax ahir <laxahir88@gmail.com>
2024-05-22 11:12:49 +0530

    ERYT

---
+++ project-1/js/swap.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+let c;
+
+console.log("a =", a);
+console.log("b =", b);
+
+c = a;
+a = b;
+b = c;
+
+console.log("after swapping :");
+
+console.log("a =", a);
+console.log("b =", b);
